49d3d0ece3648594c97ea9b1dfb1e53c355882d9
oliviernchima <nchima2010@gmail>
2018-03-06 10:29:54 +0100

    blog

---
+++ blog.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> blog </title>
+  <!-- <link rel="stylesheet" type="text/css" href="assets1/css/style.css"> -->
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<link rel="icon" type="image/png" href="assets/images/favicon.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+<link rel="stylesheet" href="assets/css/main.css"/>
+<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+<link rel="stylesheet" href="assets/css/custom.css">
+<link rel="stylesheet" href="assets/css/hover.css">
+
+</head>
+<body>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12 col-12">
+      <p>
+        <!-- Header -->
+        <?php
+          include './header.php';
+        ?>
+      </p>
+    </div>
+  </div>
+
+  <div class="row" id="first-row">
+    <div class="card mb-3" id="first-card-bgcolor">
+      <img class="card-img-top" src="assets/images/bannerlandascape.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">Quelques conseils avant de vous rendre au Repair Café</h2>
+        <p class="card-text">
+          <ul>
+           <li> Vérifiez avant tout que votre objet n'est plus sous garantie; </li>
+           <li> renez avec vous tous les accessoires de l'objet à réparer;</li>
+           <li> Si vous identifiez une pièce défectueuse, essayez d'en trouver une de remplacement car nous n'avons pas de stock</li>
+           <li> En arrivant, vous devrez vous inscrire à l'accueil et recevoir une fiche d'inscription et un numéro de passage.</li>
+           <p> <a href="#"> +infos </a> </p>
+          </ul>
+        </p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+
+<!-- -------------------------------------------------------------------------------------------------------- -->
+
+    <div class="row" id="second-row">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+          <div class="card">
+            <img class="card-img-top" src="assets/images/reparation-ordinateur.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h1 class="card-title1">ordinateur Informatique / Imprimante </h1>
+              <p class="card-text">Ordinateur portable, imprimante jet d'encre / laser / aiguille, ordinateur de bureau, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
+              <p>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in re </p>
+            </div>
+              <p class="card-text"><small class="text-muted">Last updated 4d-3h-8mins ago</small></p>
+          </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+        <div class="card">
+          <img class="card-img-top" src="assets/images/repair2.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h1 class="card-title">Electro / Electronique / Electricité</h1>
+            <p class="card-text"> Sèche-cheveux, rasoir, mixer, grille pain, four, gaufrier, jeux vidéos, pèse-personne, radio, réveil, téléphone, machine à café, senseo, magnétoscope, lecteur DVD, walkman, jouets électroniques, chaîne Hi-Fi, horloge, lampe, ...</p>
+            <p>
+            Attention, nous ne garantissons pas un accès aisé au local et donc la réparation d'objets trop encombrants  aggo eiusmod tempor (gros électro, ...)</p>
+          </div>
+            <p class="card-text"><small class="text-muted">Last updated 1d-2h-4mins ago</small></p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+        <div class="card">
+          <img class="card-img-top" src="assets/images/repair3.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h1 class="card-title"> Bois / Divers </h1>
+            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris  </p>
+
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <p class="card-text"><small class="text-muted">Last updated 12h-8mins ago</small></p>
+        </div>
+      </div>
+    </div>
+
+<!-- -----------------------------------------------------------------------------------------------------  -->
+
+    <div class="row" id="third-row">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+          <div class="card">
+            <img class="card-img-top" src="assets/images/computer.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h1 class="card-title"> Pariatur. Excepteur sint occaecat</h1>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              </p>
+              <p> reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <p class="card-text"><small class="text-muted">Last updated 45mins ago</small></p>
+          </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+        <div class="card">
+          <img class="card-img-top" src="assets/images/bike.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h1 class="card-title"> Consectetur adipisicing elit.</h1>
+            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            </p>
+            <p> voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+          </div>
+            <p class="card-text"><small class="text-muted">Last updated 18mins ago</small></p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+        <div class="card">
+          <img class="card-img-top" src="assets/images/repair4.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h1 class="card-title">Ut enim ad minim veniam, quis nostrud. </h1>
+            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </p>
+            <p> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui .</p>
+          </div>
+          <p class="card-text"><small class="text-muted">Last updated 18mins ago</small></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row" id="last-row">
+      <div class="col-12">
+        <?php
+        include './footer.php';
+        ?>
+      </div>
+    </div>
+
+  </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="fr">
+	<head>
+		<title>Repair Café Schaerbeek</title>
+		<meta charset="utf-8" />
+		<link rel="icon" type="image/png" href="assets/images/favicon.png" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<link rel="stylesheet" href="assets/css/custom.css">
+		<link rel="stylesheet" href="assets/css/hover.css">
+		<!-- DROPDOWN -->
+	</head>
+	<body class="landing">
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+				<?php
+				include './header.php';
+				?>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2>Repair Café Schaerbeek</h2>
+							<h3>Jeter ? Pas question !</h3>
+							<p>Réparer ensemble, c’est l’idée des Repair Cafés : des experts en la matière sont présents et ce chaque deuxième dimanche du mois de 14 à 17h.
+						</div>
+						<a href="#one" class="more scrolly">Learn More</a>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style1 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Prochaine session: Dimanche 11 mars dès 14h au 307, rue des Coteaux à Schaerbeek.
+								</h2>
+								<p>Avant de vous rendre au Repair Café, afin de mieux préparer votre visite et de donner plus de chance à la réparation de pouvoir être faite, nous vous proposons ces
+								<span onclick="myFunction()" class="hvr-underline-from-left dropbtn">quelques conseils.</span>
+								</p>
+							  	<div id="myDropdown" class="dropdown-content">
+									<table>
+										<tr>
+											<td>
+												Vérifiez avant tout que votre objet n'est plus sous garantie, en effet la plupart des objets bénéficient d'une garantie de 2 ans à dater le l'achat, parfois plus. Et travailler sur celui-ci annule la garantie.
+											</td>
+											<td>
+												Prenez avec vous tous les accessoires de l'objet à réparer : cordon d'alimentation, chargeur, CD d'installation, manuel d'utilisation, etc. Ainsi que tout ce qui est nécessaire pour vérifier que l'objet est réparé ou pas (CD de musique, cassette vidéo, ampoule,...).
+											</td>
+											<td>
+												Si vous identifiez une pièce défectueuse, essayez d'en trouver une de remplacement car nous n'avons pas de stock. Il y a des magasins spécialisés qui vendent des pièces de rechange comme Mister Pièces, Handyman, Elak, Veritas, ou encore les magasins d'ordinateurs, de couture, les cordonniers, les magasins de vélos. Essayez également les ressourceries.
+											</td>
+											<td>
+												En arrivant, vous devrez vous inscrire à l'accueil et recevoir une fiche d'inscription et un numéro de passage. Cela est obligatoire. Vous pouvez déjà télécharger, pré-remplir et imprimer cette fiche pour faciliter votre inscription.
+											</td>
+										</tr>
+										<tr>
+											<td>
+												Afin de ne pas pénaliser les autres visiteurs, nous ne traitons qu'un seul objet par personne inscrite.
+											</td>
+											<td>
+												Essayez de trouver via des sites comme "Comment-réparer" ,"I fix it", ou encore des forums d'entre-aide, si d'autres personnes ont le même problème que vous car cela peur fournir de précieux renseignements.
+											</td>
+											<td>
+												Afin de ne pas pénaliser les autres visiteurs, nous ne traitons qu'un seul objet par personne inscrite.
+												En vous inscrivant vous reconnaissez avoir lu et accepté les conditions de participation au Repair Café.
+											</td>
+											<td>
+												Nous prenons des photos lors de l’évènement et le Repair Café attire également des journalistes, blogueurs, gazettes etc. , vous êtes donc susceptibles d'être pris en photo/filmés lors de l’évènement. Les photos que nous prenons seront principalement utilisées sur ce site.
+											</td>
+										</tr>
+									</table>
+							  	</div>
+							</header>
+						</div>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper alt style2">
+						<h2>Nos ateliers</h2>
+						<section class="spotlight">
+							<div class="image"><img src="assets/images/couture.jpg" alt="couture" /></div><div class="content">
+								<h3>Couture</h3>
+								<p>Trous, poches trouées, fermeture éclair, coutures défaites, boutons arrachés, ... sur vos vêtements, pulls, jeans, chemises, chaussures, ...</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="assets/images/computer.jpg" alt="ordinateur" /></div><div class="content">
+								<h3>Informatique / Imprimante	</h3>
+								<p>Ordinateur portable, imprimante jet d'encre / laser / aiguille, ordinateur de bureau, ...</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="assets/images/radio.jpg" alt="radio" /></div><div class="content">
+								<h3>Electro / Electronique / Electricité</h3>
+								<p>Sèche-cheveux, rasoir, mixer, grille pain, four, gaufrier, jeux vidéos, pèse-personne, radio, réveil, téléphone, machine à café, senseo, magnétoscope, lecteur DVD, walkman, jouets électroniques, chaîne Hi-Fi, horloge, lampe, ... <br>
+								Attention, nous ne garantissons pas un accès aisé au local et donc la réparation d'objets trop encombrants (gros électro, ...)</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="assets/images/wood.jpg" alt="bois" /></div><div class="content">
+								<h3>Bois / Divers</h3>
+								<p>Chaises, jouets anciens, bibelots, bijoux, ...</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="assets/images/bike.jpg" alt="vélo" /></div><div class="content">
+								<h3>Vélo-Mécanique</h3>
+								<p>Votre vélo bien sûr ! Nous avons un atelier partenaire où vous pourrez même acheter une éventuelle pièce de rechange à prix coûtant.	</p>
+							</div>
+						</section>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Gallerie</h2>
+							</header>
+							<ul class="features">
+								<li>
+								<img src="assets/images/repair1.jpg" alt="photo d'atelier">
+								</li>
+								<li>
+								<img src="assets/images/repair2.jpg" alt="photo d'atelier">
+								</li>
+								<li>
+								<img src="assets/images/repair3.jpg" alt="photo d'atelier">
+								</li>
+								<li>
+								<img src="assets/images/repair4.jpg" alt="photo d'atelier">
+								</li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Fourth -->
+					<section id="fourth" class="wrapper style1 special">
+						<div class="inner">
+							<h2 id="FAQ">FAQ</h2>
+							<header class="major">
+								<div>
+								<h4 id="q1">Qu'est-ce qu'un Repair Café ? Qui êtes-vous ?</h4>
+								<p id="r1">Le Repair Café est un endroit où l'on peut venir réparer un objet gratuitement avec l'aide de bénévoles expert en la matière. C'est également un lieu de rencontre et d'échanges entre visiteurs et bénévoles. Notre Repair Café Schaerbeek a été créé en avril 2013 par un groupe de citoyens motivés et intéressés par le concept.
+								</p>
+							</div>
+								<h4 id="q2">Qui peut venir au Repair Café ?</h4>
+								<p id="r2">Tout le monde ! Le Repair Café est ouvert à tous, car en plus d'être un endroit où l'on répare, c'est aussi un lieu convivial où l'on peut rencontrer des personnes et apprendre et partager des connaissances.</p>
+								<h4 id="q3">Quand et où a lieu le Repair Café Schaerbeek ?</h4>
+								<p id="r3">Notre Repair Café a lieu tous les deuxièmes dimanches du mois, rue des coteaux, de 14 à 18h. Vu le succès de certains ateliers, les inscriptions sont susceptibles d'être clôturées assez tôt.
+								Il y a aussi d'autres Repair Cafés à Bruxelles et en Belgique avec des jours d'ouverture différents.</p>
+								<h4 id="q4">Que coûte une réparation ?</h4>
+								<p id="r4">C'est totalement gratuit ! Les organisateurs et réparateurs du Repair Café sont tous bénévoles. Les quelques frais d'organisation et de gestion sont financés par les dons récoltés durant l'évènement.</p>
+								<h4 id="q5">Peut-on vous soutenir ou vous aider ?</h4>
+								<p id="r5">Tout aide est toujours la bienvenue pour organiser nos ateliers, consultez notre page de soutien pour voir comment vous pouvez nous aider.</p>
+								<h4 id="q6">Quels types d'objets pouvez-vous réparer ?</h4>
+								<p id="r6">Lors de nos sessions nous avons différents ateliers de réparation pour des objets comme les appareils électriques, les vêtements ou encore les vélos. Vous trouverez plus d'informations sur les différents ateliers sur notre page de conseils pour préparer votre visite.</p>
+								<h4 id="q7">Est-ce que mon objet pourra être réparé ?</h4>
+								<p id="r7">Il n'est pas possible de savoir à l'avance si un objet est réparable. Les causes d'une même panne pouvant être multiples, seul le bénévole qui vous accueillera sur place pourra, selon ses connaissances, évaluer si l'objet est réparable. Les bénévoles du Repair Café Schaerbeek ne sont pas toujours les mêmes d'une session à l'autre et chacun a son domaine d'expertise. Dans la pratique, une majorité d'objets sont en général réparés lors des sessions. Plus d'information sur les différents ateliers lors de nos session sur notre page de conseils pour préparer votre visite.</p>
+								<h4 id="q8">Puis-je prendre rendez-vous avec un réparateur ?</h4>
+								<p id="r8">Non, le Repair Café ouvre à 14h et les visiteurs sont invités aux tables de réparation dans leur ordre d'arrivée. Les files pouvant parfois êtres longues pour certains ateliers, il nous arrive d'arrêter les inscriptions plus tôt dans l'après-midi.</p>
+								<h4 id="q9">Pouvez-vous effectuer une réparation à domicile ?</h4>
+								<p id="r9">Le Repair Café ne prévoit pas cela. Tous les réparateurs du Repair Café sont des bénévoles et ne s'engagent que pour la session et ils ne sont également assurés que sur place. L'idée est de réparer ensemble, en équipe, avec les moyens du bord. Les visiteurs sont donc invités à amener leur objet lors d'une session.</p>
+								<h4 id="q10">Puis-je amener plusieurs objets à faire réparer en même temps ?</h4>
+								<p id="r10">Vu le succès du Repair Café, et pour ne pas pénaliser les autres visiteurs, nous n'inscrivons qu'un seul objet par personne à votre arrivée. Mais cela ne vous empêche pas, après votre passage à la table de réparation, de faire une nouvelle fois la file à l'inscription pour inscrire un autre objet.</p>
+							</header>
+						</div>
+					</section>
+				<!-- Footer -->
+				<?php
+				include './footer.php';
+				?>
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+			<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+			<script>
+			$(document).ready(function() {
+			  $("#q1").click(function() {
+			      $( "#r1" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q2").click(function() {
+			      $( "#r2" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q3").click(function() {
+			      $( "#r3" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q4").click(function() {
+			      $( "#r4" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q5").click(function() {
+			      $( "#r5" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q6").click(function() {
+			      $( "#r6" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q7").click(function() {
+			      $( "#r7" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q8").click(function() {
+			      $( "#r8" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q9").click(function() {
+			      $( "#r9" ).toggle( "blind", 500 );
+			    });
+			});
+			$(document).ready(function() {
+			  $("#q10").click(function() {
+			      $( "#r10" ).toggle( "blind", 500 );
+			    });
+			});
+			</script>
+			<script>
+			/* When the user clicks on the button,
+			toggle between hiding and showing the dropdown content */
+			function myFunction() {
+			    document.getElementById("myDropdown").classList.toggle("show");
+			}
+
+			// Close the dropdown if the user clicks outside of it
+			window.onclick = function(event) {
+			  if (!event.target.matches('.dropbtn')) {
+
+			    var dropdowns = document.getElementsByClassName("dropdown-content");
+			    var i;
+			    for (i = 0; i < dropdowns.length; i++) {
+			      var openDropdown = dropdowns[i];
+			      if (openDropdown.classList.contains('show')) {
+			        openDropdown.classList.remove('show');
+			      }
+			    }
+			  }
+			}
+			</script>
+	</body>
+</html>
