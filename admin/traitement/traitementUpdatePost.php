026cf77b2b24b870016e952d57f3b0b6d07c92fb
marinasvn <marina.shvanova@gmail.com>
2018-03-14 09:39:16 +0100

    Update bug is fixed

--- admin/traitement/traitementUpdatePost.php
+++ admin/traitement/traitementUpdatePost.php
@@ -9,36 +9,46 @@
     $img = $_FILES['file']['name'];
     $categorie = $_POST['categorie'];
 
-
-// get post id
-    $getPostId = $_POST['id_post'];
-// get cat id
-$id_cat = $dbc->prepare('SELECT id_categorie FROM post_categorie WHERE id_post='.$getPostId.'');
 // get user id
     $getUserId = $dbc->prepare('SELECT id_user');
         session_start();
         $user_id = $_SESSION['id_user'];
 
-// insert it in database
+// get post id OK
+    $getPostId = $_POST['id_post'];
 
-    /*$req = $dbc->prepare("UPDATE post SET title = '".$title."', text = '".$text."', img = '".$img."', date = Curdate(), categorie = '".$categorie."' WHERE id_post = '".$getPostId."' AND user_id = '".$user_id."'");*/
+// update the post in post table (without categories)
+
 $req = $dbc->prepare("UPDATE post SET title = '".$title."', text = '".$text."', img = '".$img."', date = Curdate() WHERE id_post = '".$getPostId."' AND user_id = '".$user_id."'");
     
     $req->execute();
-    $req->closeCursor();
+    $req->closeCursor();    
 
-$req2 = $dbc->prepare('UPDATE post_categorie SET id_categorie = '.$id_cat.' WHERE id_post = '.$getPostId);
+// delete from post_categorie id's
 
-foreach ($id_cat as $id) {
-    $req2->execute();
+$reqDel = $dbc->prepare('DELETE FROM post_categorie WHERE id_post = '.$getPostId.'');
+
+$reqDel->execute();
+$reqDel->closeCursor(); 
+
+
+// select id's of categories
+
+foreach ($categorie as $cat) {
+    $sql = "SELECT id_categorie FROM categorie WHERE name = :name";
+    $statement = $dbc->prepare($sql);
+    $statement->execute( array('name' => $cat) );
+    $id_cat = $statement->fetchColumn();
+    
+    $insertcategories = $dbc->prepare('INSERT INTO post_categorie(id_post, id_categorie) VALUES(:id_post, :id_categorie)');
+        
+        $insertcategories->execute(array(
+                'id_post' => $getPostId,
+                'id_categorie' => $id_cat
+            ));
+
 }
 
-
-
-
-/*header('location: ../admin.php?pageCharger=./include/connected');*/
-
-
-
+header('location: ../admin.php?pageCharger=./include/connected');
 
 ?>
